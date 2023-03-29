4156d6e758105a1799729065cd1a7853cd7b33a1
DuyDangCode <21520774@gm.uit.edu.vn>
2023-03-29 18:43:33 +0700

    Initial commit

---
+++ GUI/ViewMD/HomeViewMD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewMD
+{
+    public class HomeViewMD:ViewModelBase
+    {
+    }
+}
